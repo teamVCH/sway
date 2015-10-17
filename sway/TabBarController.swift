dfbd73cd2159d67e1be637bf23d555164dc6c195
Hina Sakazaki <hinasakazaki@gmail.com>
2015-10-17 15:58:29 -0700

    Tabview works now

---
+++ sway/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  sway
+//
+//  Created by Hina Sakazaki on 10/17/15.
+//  Copyright © 2015 VCH. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
