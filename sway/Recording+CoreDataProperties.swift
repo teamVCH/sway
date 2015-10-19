1a9715106757c399aeb72d4375e5c87d76095fee
Christopher McMahon <melophonic@gmail.com>
2015-10-18 22:08:19 -0700

    Fix model code

---
+++ sway/Recording+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Recording+CoreDataProperties.swift
+//  sway
+//
+//  Created by Christopher McMahon on 10/18/15.
+//  Copyright © 2015 VCH. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Recording {
+
+    @NSManaged var backingAudio: NSData?
+    @NSManaged var lastModified: NSDate?
+    @NSManaged var recordingAudio: NSData?
+    @NSManaged var title: String?
+
+}
