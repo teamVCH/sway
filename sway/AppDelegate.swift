9e2a0a6dcd9f2c42700d1127f5829e84810a96ea
Christopher McMahon <melophonic@gmail.com>
2015-10-23 16:56:20 -0700

    Adding NSURLCache for downloaded audio

--- sway/AppDelegate.swift
+++ sway/AppDelegate.swift
@@ -11,64 +11,71 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
-  var window: UIWindow?
-  
-  let parseCredentials = Credentials.parseCredentials
-  let twitterCredentials = Credentials.twitterCredentials
-  
-  struct Credentials {
-    static let parseCredentialsFile = "ParseCredentials"
-    static let parseCredentials     = Credentials.loadFromPropertyListNamed(parseCredentialsFile)
-    static let twitterCredentialsFile = "TwitterCredentials"
-    static let twitterCredentials     = Credentials.loadFromPropertyListNamed(twitterCredentialsFile)
     
-    let consumerKey: String
-    let consumerSecret: String
+    var window: UIWindow?
+
+    var sharedCache: NSURLCache?
     
-    private static func loadFromPropertyListNamed(name: String) -> Credentials {
-      let path           = NSBundle.mainBundle().pathForResource(name, ofType: "plist")!
-      let dictionary     = NSDictionary(contentsOfFile: path)!
-      let consumerKey    = dictionary["Key"] as! String
-      let consumerSecret = dictionary["Secret"] as! String
-      
-      return Credentials(consumerKey: consumerKey, consumerSecret: consumerSecret)
+    let parseCredentials = Credentials.parseCredentials
+    let twitterCredentials = Credentials.twitterCredentials
+    
+    struct Credentials {
+        static let parseCredentialsFile = "ParseCredentials"
+        static let parseCredentials     = Credentials.loadFromPropertyListNamed(parseCredentialsFile)
+        static let twitterCredentialsFile = "TwitterCredentials"
+        static let twitterCredentials     = Credentials.loadFromPropertyListNamed(twitterCredentialsFile)
+        
+        let consumerKey: String
+        let consumerSecret: String
+        
+        private static func loadFromPropertyListNamed(name: String) -> Credentials {
+            let path           = NSBundle.mainBundle().pathForResource(name, ofType: "plist")!
+            let dictionary     = NSDictionary(contentsOfFile: path)!
+            let consumerKey    = dictionary["Key"] as! String
+            let consumerSecret = dictionary["Secret"] as! String
+            
+            return Credentials(consumerKey: consumerKey, consumerSecret: consumerSecret)
+        }
     }
-  }
-  
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    //set colors
-    UINavigationBar.appearance().barTintColor = UIColor(hue: 0.4722, saturation: 0.2, brightness: 0.48, alpha: 1.0) /* #627b77 */
-    UITabBar.appearance().barTintColor = UIColor(hue: 0.4722, saturation: 0.2, brightness: 0.48, alpha: 1.0) /* #627b77 */
     
-    Parse.setApplicationId(parseCredentials.consumerKey, clientKey: parseCredentials.consumerSecret)
-    PFTwitterUtils.initializeWithConsumerKey(twitterCredentials.consumerKey, consumerSecret: twitterCredentials.consumerSecret)
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        //set colors
+        UINavigationBar.appearance().barTintColor = UIColor(hue: 0.4722, saturation: 0.2, brightness: 0.48, alpha: 1.0) /* #627b77 */
+        UITabBar.appearance().barTintColor = UIColor(hue: 0.4722, saturation: 0.2, brightness: 0.48, alpha: 1.0) /* #627b77 */
+        
+        Parse.setApplicationId(parseCredentials.consumerKey, clientKey: parseCredentials.consumerSecret)
+        PFTwitterUtils.initializeWithConsumerKey(twitterCredentials.consumerKey, consumerSecret: twitterCredentials.consumerSecret)
+        
+        // initialze shared cache with 2 MB mem and 100 MB disk space
+        sharedCache = NSURLCache(memoryCapacity: 2 * 1024 * 1024, diskCapacity: 100 * 1024 * 1024, diskPath: nil)
+        NSURLCache.setSharedURLCache(sharedCache!)
+        
+        
+        return true
+    }
     
-    return true
-  }
-  
-  func applicationWillResignActive(application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-  }
-  
-  func applicationDidEnterBackground(application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-  
-  func applicationWillEnterForeground(application: UIApplication) {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-  }
-  
-  func applicationDidBecomeActive(application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-  
-  func applicationWillTerminate(application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-  
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
     // get a URL for the saved recording in the app documents folder
     static func getDocumentsFolderUrl() -> NSURL? {
         let fileManager = NSFileManager.defaultManager()
@@ -149,6 +156,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
-  
+    
 }
 
